683380dd8b6f1a20a16de9aefeb96c2162507a80
Thomas Nichols <thomas@nexus10.com>
2021-03-18 19:29:21 +0000

    Attempting JSON3-only approach

--- src/gk2obs.jl
+++ src/gk2obs.jl
@@ -1,16 +1,15 @@
 module gk2obs
 
 import Pkg;
-Pkg.add("DataFrames");
 Pkg.add("JSON3");
 
-using DataFrames, JSON3
+using JSON3
 
-df = JSON3.read.(eachline("/home/thomasn/jdi/gk2obs/sample.json")) |> DataFrame;
+json = JSON3.read.(eachline("/home/thomasn/jdi/gk2obs/sample.json")) ;
 
 i = 1;
 
-df2 = (df[i, :title], df[i, :textContent] for i in 1:size(df, 1));
+print (jsondxif2 = (df[i, :title], df[i, :textContent] for i in 1:size(df, 1));
 
 print(df2);
 
