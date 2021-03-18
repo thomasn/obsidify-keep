4fabdb18763088012fb86db8b221a0768abf4654
Thomas Nichols <thomas@nexus10.com>
2021-03-18 23:38:17 +0000

    Basic DataFrame functionality

--- src/gk2obs.jl
+++ src/gk2obs.jl
@@ -1,21 +1,18 @@
 module gk2obs
 
 import Pkg;
+Pkg.add("DataFrames");
 Pkg.add("JSON3");
 
-using JSON3
+using DataFrames, JSON3
 
-json = JSON3.read.(eachline("/home/thomasn/jdi/gk2obs/sample.json")) ;
+df = JSON3.read.(eachline("/home/thomasn/jdi/gk2obs/sample.json")) |> DataFrame;
 
-i = 1;
-
-print (jsondxif2 = (df[i, :title], df[i, :textContent] for i in 1:size(df, 1));
-
-print(df2);
-
-greet() = print("Hello World!")
-
-
-boogie() = print("And thanks for all the salmon")
+for r in eachrow(df)
+	println("== ", r[:title], "==\n");
+	# println(r.title(), "\n", r.textContent());
+	println("== LENGTH: ", length(r[:textContent]), "==\n");
+end	
 
 end # module
+
