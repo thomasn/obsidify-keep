230323c9e4bc9ce1dc1c7d30efa5328073ff3c39
Thomas Nichols <thomas@nexus10.com>
2021-03-19 02:00:45 +0000

    Moving sample file

--- src/gk2obs.jl
+++ src/gk2obs.jl
@@ -9,8 +9,8 @@ using DataFrames, JSON3
 df = JSON3.read.(eachline("/home/thomasn/jdi/gk2obs/sample.json")) |> DataFrame;
 
 for r in eachrow(df)
-	println("== ", r[:title], "==\n");
-	# println(r.title(), "\n", r.textContent());
+	println("== TITLE : ", r[:title], "==\n");
+	println("== TEXT  : ", r[:textContent], "==\n");
 	println("== LENGTH: ", length(r[:textContent]), "==\n");
 end	
 
