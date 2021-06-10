70baafeb0d3d3e225add83aa4538aca9d1caec56
Thomas Nichols <thomas@nexus10.com>
2021-06-10 23:46:12 +0100

    Exploring guarded derefs

--- src/gk2obs.jl
+++ src/gk2obs.jl
@@ -10,25 +10,38 @@ import Dates;
 Pkg.add("DataFrames");
 Pkg.add("JSON3");
 
-using DataFrames, JSON3, Dates;
+using Debugger, Printf, DataFrames, JSON3, Dates;
 
-df = JSON3.read.(eachline("/home/thomasn/jdi/gk2obs/sample.json")) |> DataFrame;
 
-for r in eachrow(df)
+function getstring(row::DataFrameRow, key::Symbol) ::String
+    key in keys(row) ? row[key] : "***"
+end
+
+
+
+function chomp_files()
+  df = JSON3.read.(eachline("/home/thomasn/jdi/gk2obs/sample.json")) |> DataFrame;
+
+  for r in eachrow(df)
 	# Don't even think about UTF-8 and graphemes...
 	unixdate_seconds = r[:userEditedTimestampUsec] / 10^6; 
+	@bp
+        @printf("------- color: %s --------\n", getstring(r, :color));
+	@bp
 	datetime = Dates.unix2datetime(unixdate_seconds);
 	ymddate = Dates.format(datetime, "yyyy-mm-dd");
-	println("== DATE  : ", ymddate);
-	println("== TITLE : ", r[:title], "==");
-	println("== TEXT  : \n", r[:textContent], "\n\n");
-	println("== LENGTH: ", length(r[:textContent]), "==\n");
-	println("== TITLE : ", r[:title], "==");
-	println("== TRASH : ", r[:isTrashed], "==");
-	println("== ARCHIV: ", r[:isArchived], "==");
-	println("== ANNOT : ", r[:annotations], "==");
-	println("== URL   : ", r[:annotations][1][:url], "==");
-end	
-
+	println("== DATE   : ", ymddate);
+	println("== TITLE  : ", r.title, "==");
+	println("== TEXT   : \n", r[:textContent], "\n\n");
+	println("== LENGTHXX : ", length(r[:textContent]), "==\n");
+	println("== TITLE  : ", r[:title], "==");
+	println("== TRASH  : ", r[:isTrashed], "==");
+	println("== ARCHIVE: ", r[:isArchived], "==");
+#	println("== ANNOT  : ", r[:annotations], "==");
+#	println("== URL    : ", r[:annotations][1][:url], "==");
+  end	
+end # function
 end # module
+
+gk2obs.chomp_files()
 
